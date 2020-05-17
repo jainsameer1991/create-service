0633c07cede07fa0d9dfb81a5063a4fbc648ab24
sameer.jain <sameer.jain@flipkart.com>
2020-05-17 18:59:10 +0530

    Created repo

---
+++ src/main/java/com/propertydekho/createservice/CreateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.propertydekho.createservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/propertydekho/createservice/CreateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propertydekho.createservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
